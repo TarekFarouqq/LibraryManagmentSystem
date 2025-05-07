616be7ddb8ff96d71499ec42e029b16f9754dbea
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-05-07 17:39:03 +0300

    Add project files.

---
+++ LibraryManagmentSystem.BLL/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace LibraryManagmentSystem.BLL.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+    } // Define your mapping configurations here  
+       
+}
