35c0a18d8fecb66100a659ada1c53a6a5985ec9b
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-05-07 18:48:12 +0300

    Setting the structure of Each Project in the Solution and Creating the Models , Repos and Services

---
+++ LibraryManagmentSystem.DAL/Interfaces/Interfaces.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagmentSystem.Models;
+
+namespace LibraryManagmentSystem.DAL.Interfaces
+{
+       public class Interfaces
+    {
+        public interface IAuthorRepository
+        {
+            Task AddAsync(Author author);
+            Task UpdateAsync(Author author);
+            Task DeleteAsync(int id);
+            Task<Author?> GetByIdAsync(int id);
+            Task<List<Author>> GetAllAsync();
+            
+        }
+
+
+        public interface IBookRepository
+        {
+            Task AddAsync(Author author);
+            Task UpdateAsync(Author author);
+            Task DeleteAsync(int id);
+            Task<Author?> GetByIdAsync(int id);
+            Task<List<Author>> GetAllAsync();
+
+        }
+
+
+    }
+}
---
+++ LibraryManagmentSystem.Models/BorrowTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentSystem.Models
+{
+     public class BorrowTransaction
+    {
+        public int Id { get; set; }
+        public DateTime BorrowedDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+
+        public int BookId { get; set; }
+
+        [ForeignKey("BookId")]
+        public Book? Book { get; set; }
+    }
+}
